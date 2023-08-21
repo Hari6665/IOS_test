2574daaf732c024474777887f3cc886993f170b7
Harikrishnan K M <harikunjath407@gmail.com>
2023-08-21 18:57:35 +0530

    Initial Commit

---
+++ LoginPortal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginPortal
+//
+//  Created by Harikrishnan K M on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
